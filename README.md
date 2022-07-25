4b85484716f1ea2387bc4d7aca6beb962f8a469b
Cosmic <jadenweong@gmail.com>
2022-07-25 23:15:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Replit Live Follower Count
+This is a project based on [Gautam](https://replit.com/@CoderGautamYT)'s [Replit Live Follower Count](https://replit.com/@CoderGautamYT/Replit-Live-Follower-Count). The main difference is that our project shows the following count for any user instead of just one. 
+
+While the code works, it doesn't necessarily work *well*. I haven't coded in a while, and others that helped don't know Python. 
+
+
+## Credits
+* [Cosmic](https://replit.com/@CosmicBear) | Developer
+* [PikachuB2005](https://replit.com/@PikachuB2005) | Helper
+
+Special Thanks to
+* [Coder Gautam](https://replit.com/@CoderGautamYT)
+* [Buddy](https://replit.com/@bddy)
+* [Icemaster](https://replit.com/@IcemasterEric)
+
+## To do:
+2. Logs
+3. Add login system
+4. View who has searched for you
+5. Fix api
+6. Integrate graphql
---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from colors import *
+
+
+import threading
+
+global yo;
+yo = 0;
+
+def printit(username):
+  threading.Timer(2.0, printit,).start()
+  profile = requests.get(f"https://replit.com/@{username}").text
+  global yo;
+  
+  yo = profile[profile.find('followerCount')+15: profile.find('followerCount')+30].lower().replace('a','').replace('b','').replace('c','').replace('d','').replace('e','').replace('f','').replace('g','').replace('h','').replace('i','').replace('j','').replace('k','').replace('l','').replace('m','').replace('n','').replace('o','').replace('p','').replace('q','').replace('r','').replace('s','').replace('t','').replace('u','').replace('v','').replace('w','').replace('x','').replace('y','').replace('z','').replace(',','').replace('"','')
+  
+  print(f"{bright_blue}e {Blue}e{bright_blue}:{bright_yellow}",yo,reset,"\n")
+
+
+from flask import Flask, send_file, redirect, render_template, request
+
+template_dir = os.path.abspath('./')
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route("/api/<username>")
+def api(username):
+    printit(username)
+    print(f"REQUEST: {username}")
+    with open("log.txt","a") as f:
+        f.write(f"\nRequest: {username}")
+    global yo;
+    return str(yo)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    if request.method == "POST":
+        username = request.form["username"]
+        return redirect(f'/{username}')
+    return render_template('first.html')
+@app.route("/<username>")
+def index(username):
+    if username == None:
+        return redirect('/CosmicBear')
+    #entry = os.path.join('index.html')
+    return render_template("index.html",username=username)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
